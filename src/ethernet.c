93b76aca5609efd6dd3fa441eee896d5bb6a9fa1
LQ_laptop <1264432729@qq.com>
2023-04-07 17:24:48 +0800

    lab2_eth_finish

--- src/ethernet.c
+++ src/ethernet.c
@@ -10,7 +10,16 @@
  */
 void ethernet_in(buf_t *buf)
 {
-    // TO-DO
+    // TO-DO finish
+    if(buf->len < sizeof(ether_hdr_t)){
+        return 0;
+    }
+
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+
+    buf_remove_header(buf, sizeof(ether_hdr_t));
+
+    net_in(buf, swap16(hdr->protocol16), hdr->src);
 }
 /**
  * @brief 处理一个要发送的数据包
@@ -21,7 +30,21 @@ void ethernet_in(buf_t *buf)
  */
 void ethernet_out(buf_t *buf, const uint8_t *mac, net_protocol_t protocol)
 {
-    // TO-DO
+    // TO-DO finish
+    if(buf->len < ETHERNET_MIN_TRANSPORT_UNIT){
+        buf_add_padding(buf, ETHERNET_MIN_TRANSPORT_UNIT - buf->len);
+    }
+
+    buf_add_header(buf, sizeof(ether_hdr_t));
+
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+    memcpy(hdr->dst, mac, NET_MAC_LEN);
+    memcpy(hdr->src, net_if_mac, NET_MAC_LEN);
+    // *hdr->dst = *mac;
+    // *hdr->src = *net_if_mac;
+    hdr->protocol16 = swap16(protocol);
+
+    driver_send(buf);
 }
 /**
  * @brief 初始化以太网协议
